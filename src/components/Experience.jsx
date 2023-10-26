da9955889cb4371e80dea0ecfda9ce8733e20f78
Dominic Essien <essiendominic1@gmail.com>
2023-10-26 15:08:14 +0100

    temp log camera position

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,18 @@
-import { OrbitControls } from "@react-three/drei";
+import React from "react";
 import BoxCover from "./BoxCover";
 import Lights from "./Lights";
 import Iphone from "./Iphone";
+import { useThree } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export const Experience = () => {
+  const camera = useThree((state) => state.camera);
+
+  function logCameraPosition() {
+    console.log(camera);
+  }
+  window.addEventListener("click", logCameraPosition);
+
   return (
     <>
       <OrbitControls />
