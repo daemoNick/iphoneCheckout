4dc7d9fe4d57f0931d13a842a52302c199b5a026
Dominic Essien <essiendominic1@gmail.com>
2024-08-08 04:15:15 +0100

    feat: remove dead code

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import studio from "@theatre/studio";
-import extension from "@theatre/r3f/dist/extension";
-
-studio.extend(extension);
-studio.initialize();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Suspense fallback={null}>
